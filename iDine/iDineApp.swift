66997fe96cc1335b45880def403dd3531d4ec998
Jeevan Joshi <joshijeevan98@gmail.com>
2022-12-23 12:53:28 +0530

    initial commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Jeevan Chandra Joshi on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
